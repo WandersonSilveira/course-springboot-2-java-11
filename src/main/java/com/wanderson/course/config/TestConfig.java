8dd795875c7c9af047eeaeb37f7f242dd82b2259
WandersonSilveira <wanderson.silveira23@gmail.com>
2022-07-25 14:17:34 -0300

    Order, instant, ISO 8601

--- src/main/java/com/wanderson/course/config/TestConfig.java
+++ src/main/java/com/wanderson/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.wanderson.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.wanderson.course.entities.Order;
 import com.wanderson.course.entities.User;
+import com.wanderson.course.repositories.OrderRepository;
 import com.wanderson.course.repositories.UserRepository;
 
 @Configuration
@@ -16,14 +19,24 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
 		User user1 = new User(1L, "Maria", "maria@gmail.com", "1111-1111", "12345");
 		User user2 = new User(2L, "Joao", "joao@gmail.com", "2222-2222", "67890");
 		
 		userRepository.saveAll(Arrays.asList(user1, user2));
+		
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
+		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
+		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+		
+		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 
 	}
 }
